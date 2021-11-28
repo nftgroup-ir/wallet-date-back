c0410586afaa7b8117b982356f6438e01a606028
FatemehDerakhshan <fatimagranger@yahoo.com>
2021-11-28 12:38:07 +0330

    get nft & transaction in csv serializer

--- csvmanager/serializer.py
+++ csvmanager/serializer.py
@@ -7,20 +7,40 @@ from rest_framework_bulk import (
 )
 
 
+class TransactionSerializer(ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = '__all__'
+
 class BalanceDataSerializer(ModelSerializer):
     class Meta:
         model = BalanceData
         fields = '__all__'
+
         
 class CSVserializer(BulkSerializerMixin, ModelSerializer):
     balancedata = serializers.SerializerMethodField()
+    transaction = serializers.SerializerMethodField()
+    nft = serializers.SerializerMethodField()
+
     class Meta(object):
         model = CSV
         list_serializer_class = BulkListSerializer
         fields = '__all__'
+
     def get_balancedata(self, id):
-        balanceofdata = BalanceData.objects.filter(parent = id).distinct()
+        balanceofdata = BalanceData.objects.filter(parent=id).distinct()
         return BalanceDataSerializer(balanceofdata, many=True).data
+
+    def get_transaction(self, id):
+        transactions = Transaction.objects.filter(parent=id).distinct()
+        return TransactionSerializer(transactions, many=True).data
+
+    def get_nft(self, id):
+        nftofwallet = NFT.objects.filter(parent=id).distinct()
+        return  NFTSerializer(nftofwallet, many=True, read_only = True).data
+
+
 
 
 class LotterySerializer(ModelSerializer):
@@ -29,10 +49,7 @@ class LotterySerializer(ModelSerializer):
         fields = '__all__'
 
 
-class TransactionSerializer(ModelSerializer):
-    class Meta:
-        model = Transaction
-        fields = '__all__'
+
 
 class NFTSerializer(ModelSerializer):
     class Meta:
