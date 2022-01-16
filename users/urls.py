4c8369fe51715c2de77716e48aa9d2a13b1784a3
mhmmdhajizade <87321633+mhmmdhajizade@users.noreply.github.com>
2022-01-16 12:20:56 +0330

    AddingRecaptcha

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import include, path
+from . import views
 
 
 urlpatterns = [
     path('auth/', include('rest_auth.urls')),
-    path('auth/register/', include('rest_auth.registration.urls'))
+    path('auth/register/', include('rest_auth.registration.urls')),
+    path('auth/captchVerify/', views.recaptcha , name = 'CaptchaVerify')
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+import requests
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(['POST'])
+def recaptcha(request):
+    r = requests.post(
+      'https://www.google.com/recaptcha/api/siteverify',
+      data={
+        'secret': '6LcFmBQeAAAAAHsoXFfXbicFHU_uCN2YXb0gMies',
+        'response': request.data['captcha_value'],
+      }
+    )
+
+    return Response({'captcha': r.json()})
+
